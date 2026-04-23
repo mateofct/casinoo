1081113793058f7536abad1f01fe57946f5000be
mateo <fct.matteo@gmail.com>
2026-04-22 22:37:00 -0400

    añadida clase VentanaRuleta y configurados los imports que necesita.

---
+++ src/visual/VentanaRuleta.java
@@ -0,0 +1,13 @@
+package visual;
+
+import controller.SessionController;
+import controller.RuletaController;
+import model.Resultado;
+import model.TipoApuesta;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaRuleta {
+}
