aba34b12e76356befed2b2eaa968b2828803bffb
mateo <fct.matteo@gmail.com>
2026-04-22 22:33:59 -0400

    añadido método de apuesta.

--- src/controller/RuletaController.java
+++ src/controller/RuletaController.java
@@ -19,4 +19,18 @@ public class RuletaController {
         modeloRuleta.deposito(monto);
     }
 
+    public Resultado realizarApuesta(int monto, TipoApuesta tipo){
+        if (monto <= 0) {
+            throw new IllegalArgumentException("La apuesta no puede ser 0.");
+        }
+        if (monto > getSaldoActual()) {
+            throw new IllegalArgumentException("Saldo insuficiente.");
+        }
+
+        int numeroGanador = modeloRuleta.girarRuleta();
+        boolean ganar = modeloRuleta.evaluarResultado(numeroGanador, tipo);
+
+        Resultado resultadoRonda = new Resultado(numeroGanador, tipo, monto, ganar);
+        return resultadoRonda;
+    }
 }
