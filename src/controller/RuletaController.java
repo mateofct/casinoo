ffa5021028e11cf979d9bab22873d50e28db22e7
mateo <fct.matteo@gmail.com>
2026-04-22 22:29:47 -0400

    inicializar ruleta con monto inicial y mostrar el saldo a la ventana de forma segura

--- src/controller/RuletaController.java
+++ src/controller/RuletaController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import model.Resultado;
+import model.Ruleta;
+import model.TipoApuesta;
+
 public class RuletaController {
+    private Ruleta modeloRuleta;
+
+    public RuletaController(){
+        this.modeloRuleta = new Ruleta(500); //consultar cuanto tiene q ser el saldo inicial.
+    }
+
+    public int getSaldoActual(){
+        return modeloRuleta.getSaldo();
+    }
+
+    public void recargarSaldo(int monto){
+        modeloRuleta.deposito(monto);
+    }
+
 }
